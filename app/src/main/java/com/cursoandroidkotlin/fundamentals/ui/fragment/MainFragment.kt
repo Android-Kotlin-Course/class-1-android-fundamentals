d4c4a817e14cd7a5d34af6e9c61191ef60f0f469
sancarbar <sancarbar@gmail.com>
2020-02-16 14:05:02 -0500

    Added button to navigate to SecondActivity.

--- app/src/main/java/com/cursoandroidkotlin/fundamentals/ui/fragment/MainFragment.kt
+++ app/src/main/java/com/cursoandroidkotlin/fundamentals/ui/fragment/MainFragment.kt
@@ -1,11 +1,13 @@
 package com.cursoandroidkotlin.fundamentals.ui.fragment
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.cursoandroidkotlin.fundamentals.R
+import kotlinx.android.synthetic.main.fragment_main.*
 
 /**
  * @author Santiago Carrillo
@@ -19,6 +21,16 @@ class MainFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         return inflater.inflate(R.layout.fragment_main, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        startSecondActivity.setOnClickListener { onStartSecondActivityClicked() }
+    }
+
+    private fun onStartSecondActivityClicked() {
+        Log.d("Developer", "onStartSecondActivityClicked!!!")
+        //TODO 1. Start second activity
     }
 
 }
